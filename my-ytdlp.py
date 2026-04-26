72d089a865e5dc46388002ec16d5ae841fe658c3
Wiivo <ivailotashev02@gmail.com>
2026-04-26 18:05:39 +0300

    Apply reformatting

--- my-ytdlp.py
+++ my-ytdlp.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
-import argparse, os, sys
+import argparse
+import os
+import sys
 from subprocess import run
+
 
 def dir_path(path):
     if os.path.isdir(path):
@@ -8,46 +11,71 @@ def dir_path(path):
     else:
         raise argparse.ArgumentTypeError(f"{path} is not a valid path")
 
+
 def timestamp(value=None):
     if value is None:
         return None
 
     tokens = value.split("-")
-    if len(tokens) == 1 or tokens[1]=="":
+    if len(tokens) == 1 or tokens[1] == "":
         return f"{tokens[0]}-inf"
     elif len(tokens) == 2:
         return value
 
     raise ValueError()
 
+
 # hack to allow a leading dash for timestamp
 for i in range(len(sys.argv)):
-    if sys.argv[i] == '-t' or sys.argv[i] == '--trim':
-        if sys.argv[i+1][0] == '-':
-            tmp = sys.argv[i+1]
-            sys.argv[i+1] = "0:00" + tmp
+    if sys.argv[i] == "-t" or sys.argv[i] == "--trim":
+        if sys.argv[i + 1][0] == "-":
+            tmp = sys.argv[i + 1]
+            sys.argv[i + 1] = "0:00" + tmp
 
 parser = argparse.ArgumentParser(
-                    prog='my-ytdlp',
-                    description='Custom simplified ytdlp script',
-                    formatter_class=argparse.RawTextHelpFormatter
+    prog="my-ytdlp",
+    description="Custom simplified ytdlp script",
+    formatter_class=argparse.RawTextHelpFormatter,
 )
 
-parser.add_argument('url', metavar="URL", nargs='+',
-                    help='youtube url')
-parser.add_argument('-a', "--audio", action='store_true', default=False,
-                    help='flag to download audio file')
-parser.add_argument('-m', "--metadata", action='store_true', default=False,
-                    help='flag to embed metadata and thumbnail')
-parser.add_argument('-f',"--force-h264", action='store_true', default=False,
-                    help='flag to force convert to H.264')
-parser.add_argument('-i',"--items", type=str,
-                    help="items to download, for example '1:3,7,-5::2' on a playlist of size 15 will download 1,2,3,7,11,13,15")
-parser.add_argument('-p', "--path", type=dir_path, default=os.getcwd(),
-                    help='set path')
-parser.add_argument('-t' ,'--trim', type=timestamp, default=timestamp(), metavar='TIMESTAMP',
-                    help='trim download using timestamp, for example:\n-t 0:14-1:23\n-t 0:14')
-parser.add_argument('-v','--version', action='version', version='%(prog)s v1.0')
+parser.add_argument("url", metavar="URL", nargs="+", help="youtube url")
+parser.add_argument(
+    "-a",
+    "--audio",
+    action="store_true",
+    default=False,
+    help="flag to download audio file",
+)
+parser.add_argument(
+    "-m",
+    "--metadata",
+    action="store_true",
+    default=False,
+    help="flag to embed metadata and thumbnail",
+)
+parser.add_argument(
+    "-f",
+    "--force-h264",
+    action="store_true",
+    default=False,
+    help="flag to force convert to H.264",
+)
+parser.add_argument(
+    "-i",
+    "--items",
+    type=str,
+    help="items to download, for example '1:3,7,-5::2' on a playlist of size 15 will download 1,2,3,7,11,13,15",
+)
+parser.add_argument("-p", "--path", type=dir_path, default=os.getcwd(), help="set path")
+parser.add_argument(
+    "-t",
+    "--trim",
+    type=timestamp,
+    default=timestamp(),
+    metavar="TIMESTAMP",
+    help="trim download using timestamp, for example:\n-t 0:14-1:23\n-t 0:14",
+)
+parser.add_argument("-v", "--version", action="version", version="%(prog)s v1.0")
 
 args = parser.parse_args()
 
@@ -55,11 +83,23 @@ ytdlp = ["yt-dlp", *args.url, "-P", args.path, "--format", "bestvideo+bestaudio"
 
 if args.force_h264:
     if not args.audio:
-        ytdlp.extend(["--exec", "ffmpeg -i {} -c:v libx264 -c:a aac -strict -2 {}_AVC.mp4",
-         "--exec", "del {}" if sys.platform == "win32" else "rm {}"])
+        ytdlp.extend(
+            [
+                "--exec",
+                "ffmpeg -i {} -c:v libx264 -c:a aac -strict -2 {}_AVC.mp4",
+                "--exec",
+                "del {}" if sys.platform == "win32" else "rm {}",
+            ]
+        )
     else:
-        ytdlp.extend(["--exec", "ffmpeg -i {} -vn -ar 44100 -ac 2 -b:a 192k {}_AVC.mp3",
-         "--exec", "del {}" if sys.platform == "win32" else "rm {}"])
+        ytdlp.extend(
+            [
+                "--exec",
+                "ffmpeg -i {} -vn -ar 44100 -ac 2 -b:a 192k {}_AVC.mp3",
+                "--exec",
+                "del {}" if sys.platform == "win32" else "rm {}",
+            ]
+        )
 
 if args.items:
     ytdlp.extend(["-I", args.items])
--- vegas-dejunk.py
+++ vegas-dejunk.py
@@ -1,4 +1,7 @@
-import os, re, argparse
+import argparse
+import os
+import re
+
 
 def dir_path(path):
     if os.path.isdir(path):
@@ -6,18 +9,24 @@ def dir_path(path):
     else:
         raise argparse.ArgumentTypeError(f"{path} is not a valid path")
 
+
 parser = argparse.ArgumentParser(
-                    prog='vegas-dejunk',
-                    description='Delete sfk files of nonexisting (or all) videos in a directory'
+    prog="vegas-dejunk",
+    description="Delete sfk files of nonexisting (or all) videos in a directory",
 )
 
-parser.add_argument('-d', "--dry", action='store_true', default=False,
-                    help='do a dry run')
-parser.add_argument('-a', "--all", action='store_true', default=False,
-                    help='delete all sfk files, not just of nonexisting videos')
-parser.add_argument('-p', "--path", type=dir_path, default=os.getcwd(),
-                    help='set path')
-parser.add_argument('-v','--version', action='version', version='%(prog)s v1.0')
+parser.add_argument(
+    "-d", "--dry", action="store_true", default=False, help="do a dry run"
+)
+parser.add_argument(
+    "-a",
+    "--all",
+    action="store_true",
+    default=False,
+    help="delete all sfk files, not just of nonexisting videos",
+)
+parser.add_argument("-p", "--path", type=dir_path, default=os.getcwd(), help="set path")
+parser.add_argument("-v", "--version", action="version", version="%(prog)s v1.0")
 
 args = parser.parse_args()
 
@@ -26,8 +35,12 @@ dir_list = os.listdir(args.path)
 for file in dir_list:
     file_path = os.path.join(args.path, file)
     if os.path.isfile(file_path):
-        if (re.search(r'\.sfk\d?$', file) and (file[:-5] not in dir_list or args.all)) or (re.search(r'\.veg.bak$', file) and (file[:-4] not in dir_list or args.all)):
-            if (args.dry == False):
+        if (
+            re.search(r"\.sfk\d?$", file) and (file[:-5] not in dir_list or args.all)
+        ) or (
+            re.search(r"\.veg.bak$", file) and (file[:-4] not in dir_list or args.all)
+        ):
+            if not args.dry:
                 os.remove(file_path)
             print(file + " deleted")
 
